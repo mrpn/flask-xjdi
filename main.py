e38dad1cb836fcb19bb48d53cf9584cb9a06a808
mrpn <xenuan@gmail.com>
2023-02-01 16:40:48 +0100

    fix

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from flask_discord_interactions import DiscordInteractions
 app = Flask(__name__)
 discord = DiscordInteractions(app)
 
-app.config["DISCORD_CLIENT_ID"] = os.getenv["DISCORD_CLIENT_ID"]
-app.config["DISCORD_PUBLIC_KEY"] = os.getenv["DISCORD_PUBLIC_KEY"]
-app.config["DISCORD_CLIENT_SECRET"] = os.getenv["DISCORD_CLIENT_SECRET"]
+app.config["DISCORD_CLIENT_ID"] = os.environ.get("DISCORD_CLIENT_ID")
+app.config["DISCORD_PUBLIC_KEY"] = os.environ.get("DISCORD_PUBLIC_KEY")
+app.config["DISCORD_CLIENT_SECRET"] = os.environ.get("DISCORD_CLIENT_SECRET")
 
 
 @app.route('/')
@@ -25,7 +25,7 @@ def ping(ctx):
 discord.set_route("/interactions")
 
 
-discord.update_commands(guild_id=os.environ["TESTING_GUILD"])
+discord.update_commands(guild_id=os.environ.get("TESTING_GUILD"))
 
 if __name__ == '__main__':
-    app.run(debug=False, port=os.getenv("PORT", default=5000))
+    app.run(debug=False, port=os.environ.get("PORT", default=5000))
